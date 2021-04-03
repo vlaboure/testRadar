c736d64b4e898efab908fb03dce634ada370ab2d
martinPOTEZ <martin.potez62@gmail.com>
2021-04-03 17:27:04 +0200

    modif vehicul

--- src/fr/projetRadar/Vehicule.java
+++ src/fr/projetRadar/Vehicule.java
@@ -2,6 +2,7 @@ package fr.projetRadar;
 
 public class Vehicule {
 	private String plaqueImmat ;
+	private String marque;
 	private int vitesse;
 	private int acceleration;
 	private String emailConducteur;
@@ -10,12 +11,15 @@ public class Vehicule {
 	public Vehicule() {
 		
 	}
+	
 	public Vehicule(String plaqueImmat,int vitesse,int acceleration,String emailConducteur,int vitesseLimite){
 		
 	}
+	
 	public void genrationAleat() {
 		
 	}
+	
 	public void modifierVitesse() {
 		
 	}
@@ -23,36 +27,51 @@ public class Vehicule {
 	public void ralentir() {
 		
 	}
+	
+	public String getMarque() {
+		return marque;
+	}
+
+	public void setMarque(String marque) {
+		this.marque = marque;
+	}
 	public int getVitesseLimite() {
 		return vitesseLimite;
 	}
+	
 	public void setVitesseLimite(int vitesseLimite) {
 		this.vitesseLimite = vitesseLimite;
 	}
+	
 	public String getPlaqueImmat() {
 		return plaqueImmat;
 	}
+	
 	public void setPlaqueImmat(String plaqueImmat) {
 		this.plaqueImmat = plaqueImmat;
 	}
+	
 	public int getVitesse() {
 		return vitesse;
 	}
+	
 	public void setVitesse(int vitesse) {
 		this.vitesse = vitesse;
 	}
+	
 	public int getAcceleration() {
 		return acceleration;
 	}
+	
 	public void setAcceleration(int acceleration) {
 		this.acceleration = acceleration;
 	}
+	
 	public String getEmailConducteur() {
 		return emailConducteur;
 	}
+	
 	public void setEmailConducteur(String emailConducteur) {
 		this.emailConducteur = emailConducteur;
 	}
-	
-	
 }
